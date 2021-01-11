599407da294b87f107b2604885f2e8705c5f5e05
Karel Svoboda <ksonpost@googlemail.com>
2021-01-11 22:25:20 +0100

    zadani

---
+++ main.py
@@ -0,0 +1,62 @@
+# Na začátku přivítá uživatele.
+
+
+
+
+# Vyžádá si od uživatele přihlašovací jméno a heslo.
+
+
+
+
+# Zjistí, jestli zadané údaje odpovídají někomu z registrovaných uživatelů.
+
+
+
+
+# Registrováni jsou následující uživatelé:
+#| USER |   PASSWORD  |
+#-----------------------
+#| bob  |     123     |
+#| ann  |    pass123  |
+#mike | password123 |
+#| liz  |    pass123  |
+#Pokud se ti tento úkol bude zdát složitý, prověř, jestli zadané údaje jsou mezi registrovanými, ale neřeš spojení uživatel - heslo.
+
+
+
+
+# Program nechá uživatele vybrat mezi třemi texty, uloženými v proměnné TEXTS.
+
+
+
+
+# Pro vybraný text spočítá následující statistiky:
+#- počet slov,
+#- počet slov začínajících velkým písmenem,
+#- počet slov psaných velkými písmeny,
+#- počet slov psaných malými písmeny,
+#- počet čísel (ne cifer!).
+
+
+
+
+# Program zobrazí jednoduchý sloupcový graf, který bude reprezentovat četnost různých délek slov v textu. Například takto:
+# 1 * 1
+# 2 *********** 11
+# 3 *************** 15
+# 4 ********* 9
+# 5 ********** 10
+#V textu, ze kterého byl vytvořen tento graf, je tedy pouze 1 jednopísmené slovo, 11 slov složených ze dvou písmen, a tak dále.
+
+
+
+
+# Program spočítá součet všech čísel (ne cifer!) v textu. Výsledkem tohoto součtu v následujícím textu by teby bylo číslo 8500:
+#"that rises sharply some 1000 feet above
+#Twin Creek Valley to an elevation of more
+#than 7500 feet above sea level. The butte
+#is located just north of US 30N"
+
+
+
+
